12dce79a96d94b50ac17d696887f515d40432673
Bjarki Ágúst Guðmundsson <suprdewd@gmail.com>
2012-08-28 22:43:55 +0000

    Added a header file to be used from other code files.

---
+++ header.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <cstring>
+#include <fstream>
+using namespace std;
+
+#define all(o) (o).begin(), (o).end()
+#define allr(o) (o).rbegin(), (o).rend()
+#define INF 2147483647
+typedef long long ll;
+typedef pair<int, int> ii;
+template <class T> int size(T x) { return x.size(); }
