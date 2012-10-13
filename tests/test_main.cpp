caf9478b242077e78db5bdd93181176e64bfb3bf
SuprDewd <suprdewd@gmail.com>
2012-10-13 18:44:40 +0000

    Added cycle detection and more.

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 #include "number_theory_and_related/is_prime.cpp"
 #include "number_theory_and_related/gcd.cpp"
 #include "number_theory_and_related/lcm.cpp"
+#include "cycle_detection/floyds_cycle_finding_algorithm.cpp"
 
 int main()
 {
@@ -14,6 +15,7 @@ int main()
 	tester.add_test(new is_prime_test());
 	tester.add_test(new gcd_test());
 	tester.add_test(new lcm_test());
+	tester.add_test(new floyds_cycle_finding_algorithm_test());
 
 	tester.run();
 
