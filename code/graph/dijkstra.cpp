038df8040206c47a9bd07c4f25b3f40045d13510
SuprDewd <suprdewd@gmail.com>
2012-10-29 04:21:24 +0000

    Added another implementation of Dijkstra's shortest path algorithm.

--- code/graph/dijkstra.cpp
+++ code/graph/dijkstra.cpp
@@ -1,21 +1,22 @@
-#define MAXEDGES 20000
-bool done[MAXEDGES];
-
 int dijkstra(int start, int end, vvii& adj_list) {
-	memset(done, 0, MAXEDGES);
+	set<int> done;
 	priority_queue<ii, vii, greater<ii> > pq;
 	pq.push(ii(0, start));
 	
 	while (!pq.empty()) {
 		ii current = pq.top(); pq.pop();
-		done[current.second] = true;
-		
-		if (current.second == end) 
+
+		if (done.find(current.second) != done.end())
+			continue;
+
+		if (current.second == end)
 			return current.first;
 
+		done.insert(current.second);
+
 		vii &vtmp = adj_list[current.second];
-		for (vii::iterator it=vtmp.begin(); it != vtmp.end(); it++)
-			if (!done[it->second])
+		for (vii::iterator it = vtmp.begin(); it != vtmp.end(); it++)
+			if (done.find(it->second) == done.end())
 				pq.push(ii(current.first + it->first, it->second));
 	}
 	
--- tester/test_header.cpp
+++ tester/test_header.cpp
@@ -17,9 +17,17 @@ void assert_not_equal(T not_expected, T actual) {
 }
 
 void assert_true(bool actual) {
-	assert_equal(true, actual);
+	if (true != actual) {
+		cout << "Assertion failed:" << endl;
+		cout << "Expected: " << "true" << endl;
+		cout << "  Actual: " << "false" << endl;
+	}
 }
 
 void assert_false(bool actual) {
-	assert_equal(false, actual);
+	if (false != actual) {
+		cout << "Assertion failed:" << endl;
+		cout << "Expected: " << "false" << endl;
+		cout << "  Actual: " << "true" << endl;
+	}
 }
