01cf1d09a5afa8a71352648773f2d83da0620a99
SuprDewd <suprdewd@gmail.com>
2012-11-12 12:31:34 +0000

    Added topological sort

---
+++ code/graph/tsort.cpp
@@ -0,0 +1,29 @@
+void tsort_dfs(int cur, char* color, const vvi& adj, stack<int>& res, bool& has_cycle) {
+	color[cur] = 1;
+	for (int i = 0, cnt = size(adj[cur]); i < cnt; i++) {
+		int nxt = adj[cur][i];
+		if (color[nxt] == 0)
+			tsort_dfs(nxt, color, adj, res, has_cycle);
+		else if (color[nxt] == 1)
+			has_cycle = true;
+		if (has_cycle) return;
+	}
+	color[cur] = 2;
+	res.push(cur);
+}
+
+vi tsort(int n, vvi adj, bool& has_cycle) {
+	has_cycle = false;
+	stack<int> S;
+	vi res;
+	char* color = new char[n];
+	memset(color, 0, n);
+	for (int i = 0; i < n; i++) {
+		if (!color[i]) {
+			tsort_dfs(i, color, adj, S, has_cycle);
+			if (has_cycle) return res;
+		}
+	}
+	while (!S.empty()) res.push_back(S.top()), S.pop();
+	return res;
+}
---
+++ code/graph/tsort.test.cpp
@@ -0,0 +1,25 @@
+void test() {
+	vvi graph(2);
+	graph[0].push_back(1);
+	bool has_error;
+	vi res = tsort(2, graph, has_error);
+	assert_false(has_error);
+	assert_equal(0, res[0]);
+	assert_equal(1, res[1]);
+
+	vvi graph2(2);
+	graph2[0].push_back(1);
+	graph2[1].push_back(0);
+	res = tsort(2, graph2, has_error);
+	assert_true(has_error);
+
+	vvi graph3(3);
+	graph3[0].push_back(2);
+	graph3[1].push_back(0);
+	graph3[1].push_back(2);
+	res = tsort(3, graph3, has_error);
+	assert_false(has_error);
+	assert_equal(1, res[0]);
+	assert_equal(0, res[1]);
+	assert_equal(2, res[2]);
+}
