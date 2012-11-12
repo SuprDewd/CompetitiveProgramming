d8e07ca137a9f50eaf2a19a9b0ee90d4373a29e0
SuprDewd <suprdewd@gmail.com>
2012-11-12 12:11:40 +0000

    Added Kruskal's algorithm

---
+++ code/graph/kruskals_mst.test.cpp
@@ -0,0 +1,21 @@
+void test() {
+	vector<pair<int, ii> > edges;
+
+	edges.push_back(pair<int, ii>(4, ii(0, 1)));
+	edges.push_back(pair<int, ii>(9, ii(0, 2)));
+	edges.push_back(pair<int, ii>(21, ii(0, 3)));
+	edges.push_back(pair<int, ii>(4, ii(1, 0)));
+	edges.push_back(pair<int, ii>(8, ii(1, 2)));
+	edges.push_back(pair<int, ii>(17, ii(1, 3)));
+	edges.push_back(pair<int, ii>(9, ii(2, 0)));
+	edges.push_back(pair<int, ii>(8, ii(2, 1)));
+	edges.push_back(pair<int, ii>(16, ii(2, 3)));
+	edges.push_back(pair<int, ii>(21, ii(3, 0)));
+	edges.push_back(pair<int, ii>(17, ii(3, 1)));
+	edges.push_back(pair<int, ii>(16, ii(3, 2)));
+
+	vector<pair<int, ii> > res = mst(4, edges);
+	int sum = 0;
+	for (int i = 0; i < size(res); i++) sum += res[i].first;
+	assert_equal(28, sum);
+}
