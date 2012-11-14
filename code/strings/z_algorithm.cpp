70e8e84f6d70396afa579138bb89e01db7f25f92
SuprDewd <suprdewd@gmail.com>
2012-11-14 22:46:12 +0000

    Added the Z algorithm.

---
+++ code/strings/z_algorithm.cpp
@@ -0,0 +1,24 @@
+int* z_values(string s) {
+	int n = size(s);
+	int* z = new int[n];
+	int l = 0, r = 0;
+	z[0] = n;
+	for (int i = 1; i < n; i++) {
+		z[i] = 0;
+		if (i > r) {
+			l = r = i;
+			while (r < n && s[r - l] == s[r]) r++;
+			z[i] = r - l; r--;
+		} else {
+			if (z[i - l] < r - i + 1) {
+				z[i] = z[i - l];
+			} else {
+				l = i;
+				while (r < n && s[r - l] == s[r]) r++;
+				z[i] = r - l; r--;
+			}
+		}
+	}
+
+	return z;
+}
---
+++ code/strings/z_algorithm.test.cpp
@@ -0,0 +1,33 @@
+void test() {
+	int* arr;
+
+	arr = z_values("blablubli");
+	assert_equal(9, arr[0]);
+	assert_equal(0, arr[1]);
+	assert_equal(0, arr[2]);
+	assert_equal(2, arr[3]);
+	assert_equal(0, arr[4]);
+	assert_equal(0, arr[5]);
+	assert_equal(2, arr[6]);
+	assert_equal(0, arr[7]);
+	assert_equal(0, arr[8]);
+
+	delete[] arr;
+
+	arr = z_values("aaa");
+
+	assert_equal(3, arr[0]);
+	assert_equal(2, arr[1]);
+	assert_equal(1, arr[2]);
+
+	delete[] arr;
+
+	arr = z_values("bobobo");
+
+	assert_equal(6, arr[0]);
+	assert_equal(0, arr[1]);
+	assert_equal(4, arr[2]);
+	assert_equal(0, arr[3]);
+	assert_equal(2, arr[4]);
+	assert_equal(0, arr[5]);
+}
