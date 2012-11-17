ac6baf732789afac1c3818cf50cdd4e5257e6e2c
SuprDewd <suprdewd@gmail.com>
2012-11-17 18:42:11 +0000

    Made Max Flow algorithm faster.

--- code/graph/edmonds_karps.cpp
+++ code/graph/edmonds_karps.cpp
@@ -1,34 +1,62 @@
-int max_flow(int n, int s, int t, int** arr) {
-	int* back = new int[n], cur, flow = 0;
+struct mf_edge {
+	int u, v, w;
+	mf_edge* rev;
+	mf_edge(int _u, int _v, int _w, mf_edge* _rev = NULL) {
+		u = _u; v = _v; w = _w; rev = _rev;
+	}
+};
+
+int max_flow(int n, int s, int t, vii* adj) {
+	vector<mf_edge*>* g = new vector<mf_edge*>[n];
+	for (int i = 0; i < n; i++) {
+		for (int j = 0, len = size(adj[i]); j < len; j++) {
+			mf_edge *cur = new mf_edge(i, adj[i][j].first, adj[i][j].second),
+					*rev = new mf_edge(adj[i][j].first, i, 0, cur);
+			cur->rev = rev;
+			g[i].push_back(cur);
+			g[adj[i][j].first].push_back(rev);
+		}
+	}
+	int flow = 0;
+	mf_edge** back = new mf_edge*[n];
 	while (true) {
-		for (int i = 0; i < n; i++) back[i] = -1;
-		queue<int> Q;
-		Q.push(back[s] = s);
+		for (int i = 0; i < n; i++) back[i] = NULL;
+		queue<int> Q; Q.push(s);
 		while (!Q.empty()) {
-			cur = Q.front(); Q.pop();
+			int cur = Q.front(); Q.pop();
 			if (cur == t) break;
-			for (int i = 0; i < n; i++) {
-				if (arr[cur][i] > 0 && back[i] < 0) {
-					back[i] = cur;
-					Q.push(i);
+			for (int i = 0, len = size(g[cur]); i < len; i++) {
+				mf_edge* nxt = g[cur][i];
+				if (nxt->v != s && nxt->w > 0 && back[nxt->v] == NULL) {
+					back[nxt->v] = nxt;
+					Q.push(nxt->v);
 				}
 			}
 		}
-		if (back[t] < 0) break;
-		int cap = INF; cur = t;
-		while (cur != s) {
-			cap = min(cap, arr[back[cur]][cur]);
-			cur = back[cur];
+		mf_edge* cure = back[t];
+		if (cure == NULL) break;
+		int cap = INF;
+		while (true) {
+			cap = min(cap, cure->w);
+			if (cure->u == s) break;
+			cure = back[cure->u];
 		}
 		assert(cap > 0 && cap < INF);
-		cur = t;
-		while (cur != s) {
-			arr[back[cur]][cur] -= cap;
-			arr[cur][back[cur]] += cap;
-			cur = back[cur];
+		cure = back[t];
+		while (true) {
+			cure->w -= cap;
+			cure->rev->w += cap;
+			if (cure->u == s) break;
+			cure = back[cure->u];
 		}
 		flow += cap;
 	}
+	// instead of deleting g, we could also
+	// use it to get info about the actual flow
+	for (int i = 0; i < n; i++)
+		for (int j = 0, len = size(g[i]); j < len; j++)
+			delete g[i][j];
+	delete[] g;
 	delete[] back;
 	return flow;
 }
--- code/graph/edmonds_karps.test.cpp
+++ code/graph/edmonds_karps.test.cpp
@@ -1,63 +1,48 @@
 void test() {
-	int** arr = new int*[4];
-	for (int i = 0; i < 4; i++)
-	{
-		arr[i] = new int[4];
-		for (int j = 0; j < 4; j++)
-			arr[i][j] = 0;
-	}
+	/* Field testing: UVa259 */
 
-	arr[0][1] = 133700;
-	arr[0][2] = 133700;
-	arr[1][2] = 1;
-	arr[1][3] = 133700;
-	arr[2][3] = 133700;
+	vii* adj = new vii[4];
+	adj[0].push_back(ii(1, 133700));
+	adj[0].push_back(ii(2, 133700));
+	adj[1].push_back(ii(2, 1));
+	adj[1].push_back(ii(3, 133700));
+	adj[2].push_back(ii(3, 133700));
 
-	assert_equal(2 * 133700, max_flow(4, 0, 3, arr));
+	assert_equal(2 * 133700, max_flow(4, 0, 3, adj));
+	delete[] adj;
 
+	adj = new vii[5];
+	adj[1].push_back(ii(2, 100));
+	adj[1].push_back(ii(3, 50));
+	adj[2].push_back(ii(3, 50));
+	adj[2].push_back(ii(4, 50));
+	adj[2].push_back(ii(0, 50));
+	adj[3].push_back(ii(4, 100));
+	adj[4].push_back(ii(0, 125));
 
-	arr = new int*[5];
-	for (int i = 0; i < 5; i++)
-	{
-		arr[i] = new int[5];
-		for (int j = 0; j < 5; j++)
-			arr[i][j] = 0;
-	}
+	assert_equal(150, max_flow(5, 1, 0, adj));
+	delete[] adj;
 
-	arr[1][2] = 100;
-	arr[1][3] = 50;
-	arr[2][3] = 50;
-	arr[2][4] = 50;
-	arr[2][0] = 50;
-	arr[3][4] = 100;
-	arr[4][0] = 125;
+	adj = new vii[5];
+	adj[1].push_back(ii(2, 100));
+	adj[1].push_back(ii(3, 50));
+	adj[2].push_back(ii(3, 50));
+	adj[2].push_back(ii(4, 50));
+	adj[2].push_back(ii(0, 50));
+	adj[3].push_back(ii(4, 100));
+	adj[4].push_back(ii(0, 75));
 
-	assert_equal(150, max_flow(5, 1, 0, arr));
+	assert_equal(125, max_flow(5, 1, 0, adj));
+	delete[] adj;
 
-	for (int i = 0; i < 5; i++)
-		for (int j = 0; j < 5; j++)
-			arr[i][j] = 0;
+	adj = new vii[5];
+	adj[1].push_back(ii(2, 100));
+	adj[1].push_back(ii(3, 50));
+	adj[2].push_back(ii(4, 5));
+	adj[2].push_back(ii(0, 5));
+	adj[3].push_back(ii(4, 100));
+	adj[4].push_back(ii(0, 125));
 
-	arr[1][2] = 100;
-	arr[1][3] = 50;
-	arr[2][3] = 50;
-	arr[2][4] = 50;
-	arr[2][0] = 50;
-	arr[3][4] = 100;
-	arr[4][0] = 75;
-
-	assert_equal(125, max_flow(5, 1, 0, arr));
-
-	for (int i = 0; i < 5; i++)
-		for (int j = 0; j < 5; j++)
-			arr[i][j] = 0;
-
-	arr[1][2] = 100;
-	arr[1][3] = 50;
-	arr[2][4] = 5;
-	arr[2][0] = 5;
-	arr[3][4] = 100;
-	arr[4][0] = 125;
-
-	assert_equal(60, max_flow(5, 1, 0, arr));
+	assert_equal(60, max_flow(5, 1, 0, adj));
+	delete[] adj;
 }
