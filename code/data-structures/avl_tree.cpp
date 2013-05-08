120eb121935db31ad81e29aefff2970014c97bab
SuprDewd <suprdewd@gmail.com>
2013-05-08 02:29:30 +0000

    Added an AVL tree

---
+++ code/data-structures/avl_tree.cpp
@@ -0,0 +1,145 @@
+template <class T>
+class avl_tree {
+public:
+
+    struct node {
+        node *p, *l, *r;
+        int size, height;
+        T item;
+        node(const T &item, node *p = NULL) : item(item), p(p), l(NULL), r(NULL), size(1), height(0) { }
+    };
+
+    avl_tree() : root(NULL) { }
+
+    node* find(const T &item) const {
+        node *cur = root;
+        while (cur) {
+            if (cur->item < item) cur = cur->r;
+            else if (item < cur->item) cur = cur->l;
+            else break;
+        }
+
+        return cur;
+    }
+
+    node* insert(const T &item) {
+        node *prev = NULL, **cur = &root;
+        while (*cur) {
+            prev = *cur;
+            if ((*cur)->item < item) cur = &((*cur)->r);
+            else if (item < (*cur)->item) cur = &((*cur)->l);
+            else return *cur;
+        }
+
+        node *n = new node(item, prev);
+        *cur = n;
+        fix(n);
+        return n;
+    }
+
+    void erase(const T &item) { erase(find(item)); }
+    void erase(node *n) {
+        if (!n) return;
+        if (!n->l && n->r) {
+            child_leg(n->p, n) = n->r;
+            n->r->p = n->p;
+        } else if (n->l && !n->r) {
+            child_leg(n->p, n) = n->l;
+            n->l->p = n->p;
+        } else if (n->l && n->r) {
+            node *s = successor(n);
+            n->item = s->item; // TODO: don't just copy contents
+            erase(s);
+            fix(n);
+            return;
+        } else child_leg(n->p, n) = NULL;
+        fix(n->p);
+        delete n;
+    }
+
+    node* successor(node *n) const {
+        if (!n) return NULL;
+        node *cur = n->r;
+        if (!cur) return n->p;
+        while (cur->l) cur = cur->l;
+        return cur;
+    }
+
+    inline int size() const { return sz(root); }
+
+    void clear() {
+        delete_tree(root);
+        root = NULL;
+    }
+
+private:
+
+    node *root;
+
+    inline int sz(node *n) const { return n ? n->size : 0; }
+    inline int height(node *n) const { return n ? n->height : -1; }
+    inline bool left_heavy(node *n) const { return n && height(n->l) > height(n->r); }
+    inline bool right_heavy(node *n) const { return n && height(n->r) > height(n->l); }
+    inline bool too_heavy(node *n) const { return n && abs(height(n->l) - height(n->r)) > 1; }
+
+    void delete_tree(node *n) {
+        if (!n) return;
+        delete_tree(n->l);
+        delete_tree(n->r);
+        delete n;
+    }
+
+    node*& child_leg(node *p, node *c) {
+        if (!p) return root;
+        if (p->l == c) return p->l;
+        if (p->r == c) return p->r;
+        // assert(false);
+    }
+
+    void augment(node *n) {
+        if (!n) return;
+        n->size = 1 + sz(n->l) + sz(n->r);
+        n->height = 1 + max(height(n->l), height(n->r));
+    }
+
+    void left_rotate(node *n) {
+        // assert(n->r);
+        node *r = n->r;
+        r->p = n->p;
+        child_leg(n->p, n) = r;
+        n->r = r->l;
+        if (r->l) r->l->p = n;
+        r->l = n;
+        n->p = r;
+        augment(n);
+        augment(r);
+    }
+
+    void right_rotate(node *n) {
+        // assert(n->l);
+        node *l = n->l;
+        l->p = n->p;
+        child_leg(n->p, n) = l;
+        n->l = l->r;
+        if (l->r) l->r->p = n;
+        l->r = n;
+        n->p = l;
+        augment(n);
+        augment(l);
+    }
+
+    void fix(node *n) {
+        while (n) {
+            augment(n);
+            if (too_heavy(n)) {
+                if (left_heavy(n) && right_heavy(n->l)) left_rotate(n->l);
+                else if (right_heavy(n) && left_heavy(n->r)) right_rotate(n->r);
+                if (left_heavy(n)) right_rotate(n);
+                else left_rotate(n);
+                n = n->p;
+            }
+
+            n = n->p;
+        }
+    }
+};
---
+++ code/data-structures/avl_tree.test.cpp
@@ -0,0 +1,58 @@
+void test() {
+    int cnt = 10000,
+        range = 1000;
+
+    avl_tree<int> t1;
+    set<int> t2;
+
+    assert_equal(0, t1.size());
+
+    for (int i = 0; i < cnt; i++) {
+        int n = rand() % range;
+        avl_tree<int>::node *p = t1.insert(n);
+        assert_equal(n, p->item);
+        t2.insert(n);
+        assert_equal(size(t2), size(t1));
+
+        int n1 = rand() % range;
+        avl_tree<int>::node *b = t1.find(n1);
+        if (b) assert_equal(n1, b->item);
+        assert_equal(b == NULL, t2.find(n1) == t2.end());
+
+        int n2 = rand() % range;
+        t1.erase(n2);
+        t2.erase(n2);
+        assert_equal(size(t2), size(t1));
+    }
+
+    t1.clear();
+    t2.clear();
+
+    assert_equal(0, t1.size());
+
+    for (int i = 0; i < cnt; i++) {
+        int n = rand() % range;
+        avl_tree<int>::node *p = t1.insert(n);
+        assert_equal(n, p->item);
+        t2.insert(n);
+        assert_equal(size(t2), size(t1));
+
+        int n1 = rand() % range;
+        avl_tree<int>::node *b = t1.find(n1);
+        if (b) assert_equal(n1, b->item);
+        assert_equal(b == NULL, t2.find(n1) == t2.end());
+
+        int n2 = rand() % range;
+        t1.erase(n2);
+        t2.erase(n2);
+        assert_equal(size(t2), size(t1));
+    }
+
+    for (int i = 0; i < range; i++) {
+        t1.erase(i);
+        t2.erase(i);
+        assert_equal(size(t2), size(t1));
+    }
+
+    assert_equal(0, t1.size());
+}
