f90b3bf4d462b1a3d3777310b6a2b10b8bd61bf7
SuprDewd <suprdewd@gmail.com>
2013-05-12 07:30:09 +0000

    Added a faster version of Dijkstra's algorithm

---
+++ code/graph/dijkstra_fast.cpp
@@ -0,0 +1,57 @@
+#define TSWP(x, y) tmp = x, x = y, y = tmp
+#define SWP(a, b) TSWP(loc[heap[a]], loc[heap[b]]), TSWP(heap[a], heap[b])
+
+pair<int*, int*> dijkstra_fast(int n, int s, vector<ii> *adj) {
+    int *heap = new int[n],
+        *loc = new int[n],
+        *dist = new int[n],
+        *parent = new int[n],
+        tmp;
+
+    for (int i = 0; i < n; i++) {
+        heap[i] = i;
+        loc[i] = i;
+        dist[i] = INF;
+        parent[i] = -1;
+    }
+
+    SWP(0, s), dist[s] = 0;
+    for (int left = n; left > 0; ) {
+        int cur = heap[0];
+        if (dist[cur] == INF) break;
+        SWP(0, left-1), left--;
+        int at = 0;
+        while (at < left) {
+            int l = 2*at + 1, r = l + 1, nxt = -1, ad = dist[heap[at]];
+            if (r >= left) {
+                if (l < left && ad > dist[heap[l]]) nxt = l;
+            } else {
+                int ld = dist[heap[l]], rd = dist[heap[r]];
+                if (ad > ld && ad > rd) nxt = ld <= rd ? l : r;
+                else if (ad > ld) nxt = l;
+                else if (ad > rd) nxt = r;
+            }
+            if (nxt != -1) SWP(at, nxt), at = nxt;
+            else break;
+        }
+
+        for (int i = 0, cnt = size(adj[cur]); i < cnt; i++) {
+            int nxt = adj[cur][i].first,
+                ndist = dist[cur] + adj[cur][i].second;
+            if (ndist < dist[nxt]) {
+                dist[nxt] = ndist;
+                parent[nxt] = cur;
+                int at = loc[nxt];
+                while (at > 0) {
+                    int p = at/2;
+                    if (dist[nxt] < dist[heap[p]]) SWP(at, p), at = p;
+                    else break;
+                }
+            }
+        }
+    }
+
+    delete[] heap;
+    delete[] loc;
+    return pair<int*, int*>(dist, parent);
+}
---
+++ code/graph/dijkstra_fast.test.cpp
@@ -0,0 +1,165 @@
+pair<int*, int*> res;
+vector<ii> *adj;
+
+void test_1() {
+    int n = 3;
+    adj = new vector<ii>[n];
+    adj[0].push_back(ii(1, 10));
+    adj[0].push_back(ii(2, 56));
+    adj[0].push_back(ii(0, 2));
+    adj[1].push_back(ii(2, 23));
+    adj[1].push_back(ii(0, 3));
+    adj[2].push_back(ii(0, 4));
+    adj[2].push_back(ii(1, 5));
+    adj[2].push_back(ii(2, 1));
+    res = dijkstra_fast(n, 0, adj);
+    assert_equal(0, res.first[0]);
+    assert_equal(-1, res.second[0]);
+    assert_equal(10, res.first[1]);
+    assert_equal(0, res.second[1]);
+    assert_equal(33, res.first[2]);
+    assert_equal(1, res.second[2]);
+    delete[] res.first;
+    delete[] res.second;
+
+	res = dijkstra_fast(n, 1, adj);
+	assert_equal(0, res.first[1]);
+	assert_equal(-1, res.second[1]);
+	assert_equal(3, res.first[0]);
+	assert_equal(1, res.second[0]);
+    delete[] res.first;
+    delete[] res.second;
+    delete[] adj;
+}
+
+void test_2() {
+	int n = 2;
+    adj = new vector<ii>[n];
+	adj[0].push_back(ii(1, 42));
+
+	res = dijkstra_fast(n, 0, adj);
+	assert_equal(42, res.first[1]);
+	assert_equal(0, res.second[1]);
+	assert_equal(0, res.first[0]);
+	assert_equal(-1, res.second[0]);
+    delete[] res.first;
+    delete[] res.second;
+
+	res = dijkstra_fast(n, 1, adj);
+	assert_equal(-1, res.second[0]);
+	assert_equal(INF, res.first[0]);
+	assert_equal(0, res.first[1]);
+	assert_equal(-1, res.second[1]);
+    delete[] res.first;
+    delete[] res.second;
+    delete[] adj;
+}
+
+void test_3() {
+	int n = 2;
+    adj = new vector<ii>[n];
+	adj[0].push_back(ii(1, 42));
+	adj[1].push_back(ii(0, 42));
+
+	res = dijkstra_fast(n, 0, adj);
+	assert_equal(42, res.first[1]);
+	assert_equal(0, res.second[1]);
+	assert_equal(0, res.first[0]);
+	assert_equal(-1, res.second[0]);
+    delete[] res.first;
+    delete[] res.second;
+
+	res = dijkstra_fast(n, 1, adj);
+	assert_equal(0, res.first[1]);
+	assert_equal(-1, res.second[1]);
+	assert_equal(42, res.first[0]);
+	assert_equal(1, res.second[0]);
+    delete[] res.first;
+    delete[] res.second;
+    delete[] adj;
+}
+
+void test_4() {
+	int n = 4;
+    adj = new vector<ii>[n];
+	adj[0].push_back(ii(1, 5));
+	adj[1].push_back(ii(2, 8));
+	adj[1].push_back(ii(3, 30));
+	adj[2].push_back(ii(3, 7));
+
+	res = dijkstra_fast(n, 0, adj);
+	assert_equal(5, res.first[1]);
+	assert_equal(0, res.second[1]);
+	assert_equal(13, res.first[2]);
+	assert_equal(1, res.second[2]);
+	assert_equal(20, res.first[3]);
+	assert_equal(2, res.second[3]);
+    delete[] res.first;
+    delete[] res.second;
+
+	res = dijkstra_fast(n, 3, adj);
+	assert_equal(INF, res.first[0]);
+	assert_equal(-1, res.second[0]);
+	assert_equal(INF, res.first[1]);
+	assert_equal(-1, res.second[1]);
+	assert_equal(INF, res.first[2]);
+	assert_equal(-1, res.second[2]);
+    delete[] res.first;
+    delete[] res.second;
+
+	res = dijkstra_fast(n, 1, adj);
+	assert_equal(15, res.first[3]);
+    delete[] res.first;
+    delete[] res.second;
+    delete[] adj;
+}
+
+void test_5() {
+	int n = 4;
+    adj = new vector<ii>[n];
+	adj[0].push_back(ii(1, 4));
+	adj[0].push_back(ii(2, 5));
+	adj[1].push_back(ii(3, 2));
+	adj[2].push_back(ii(1, -5));
+
+	res = dijkstra_fast(n, 0, adj);
+	assert_equal(0, res.first[0]);
+	assert_equal(5, res.first[2]);
+	assert_equal(0, res.second[2]);
+	assert_equal(0, res.first[1]);
+	assert_equal(2, res.second[1]);
+	assert_equal(2, res.first[3]);
+	assert_equal(1, res.second[3]);
+    delete[] res.first;
+    delete[] res.second;
+    delete[] adj;
+}
+
+void test_6() {
+	int n = 2;
+    adj = new vector<ii>[n];
+
+	res = dijkstra_fast(n, 0, adj);
+	assert_equal(-1, res.second[1]);
+	assert_equal(0, res.first[0]);
+	assert_equal(-1, res.second[0]);
+    delete[] res.first;
+    delete[] res.second;
+
+	res = dijkstra_fast(n, 1, adj);
+	assert_equal(-1, res.second[0]);
+	assert_equal(0, res.first[1]);
+	assert_equal(-1, res.second[1]);
+    delete[] res.first;
+    delete[] res.second;
+    delete[] adj;
+}
+
+void test() {
+    test_1();
+    test_2();
+    test_3();
+    test_4();
+    test_5();
+    test_6();
+}
