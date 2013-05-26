3c45697ddab3486afe9d01646892a2830a1504fc
SuprDewd <suprdewd@gmail.com>
2013-05-26 01:14:41 +0000

    Updated Bellman Ford

--- code/graph/bellman_ford.cpp
+++ code/graph/bellman_ford.cpp
@@ -1,16 +1,15 @@
 int* bellman_ford(int n, int s, vii* adj, bool& has_negative_cycle) {
-	has_negative_cycle = false;
-	int* dist = new int[n];
-	for (int i = 0; i < n; i++)
-		dist[i] = INF;
-	dist[s] = 0;
-	for (int i = 0; i < n - 1; i++)
-		for (int j = 0; j < n; j++)
-			for (int k = 0, len = size(adj[j]); k < len; k++)
-				dist[adj[j][k].first] = min(dist[adj[j][k].first], dist[j] + adj[j][k].second);
-	for (int j = 0; j < n; j++)
-		for (int k = 0, len = size(adj[j]); k < len; k++)
-			if (dist[j] + adj[j][k].second < dist[adj[j][k].first])
-				has_negative_cycle = true;
-	return dist;
+    has_negative_cycle = false;
+    int* dist = new int[n];
+    for (int i = 0; i < n; i++) dist[i] = i == s ? 0 : INF;
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n; j++)
+            if (dist[j] != INF)
+                for (int k = 0, len = size(adj[j]); k < len; k++)
+                    dist[adj[j][k].first] = min(dist[adj[j][k].first], dist[j] + adj[j][k].second);
+    for (int j = 0; j < n; j++)
+        for (int k = 0, len = size(adj[j]); k < len; k++)
+            if (dist[j] + adj[j][k].second < dist[adj[j][k].first])
+                has_negative_cycle = true;
+    return dist;
 }
