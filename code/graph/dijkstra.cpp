05aed9b6c95c0d3e5bccc17dfe741a6a3c695d16
SuprDewd <suprdewd@gmail.com>
2013-07-06 03:51:58 +0000

    Simplified Dijkstra's algorithm

--- code/graph/dijkstra.cpp
+++ code/graph/dijkstra.cpp
@@ -1,23 +1,21 @@
-map<int, ii> dijkstra_path(int start, vvii& adj_list) {
-    map<int, ii> parent;
-    priority_queue<ii, vii, greater<ii> > pq;
-    pq.push(ii(0, start));
-    parent[start] = ii(0, start);
+int *dist, *dad;
+struct cmp {
+    bool operator()(int a, int b) { return dist[a] != dist[b] ? dist[a] < dist[b] : a < b; }
+};
 
+pair<int*, int*> dijkstra_fast(int n, int s, vector<ii> *adj) {
+    dist = new int[n];
+    dad = new int[n];
+    for (int i = 0; i < n; i++) dist[i] = INF, dad[i] = -1;
+    set<int, cmp> pq;
+    dist[s] = 0, pq.insert(s);
     while (!pq.empty()) {
-        ii cur = pq.top(); pq.pop();
-
-        if (cur.first > parent[cur.second].first)
-            continue;
-
-        vii &vtmp = adj_list[cur.second];
-        for (vii::iterator it = vtmp.begin(); it != vtmp.end(); it++) {
-            if (parent.find(it->second) == parent.end() || parent[it->second].first > cur.first + it->first) {
-                parent[it->second] = ii(cur.first + it->first, cur.second);
-                pq.push(ii(cur.first + it->first, it->second));
-            }
+        int cur = *pq.begin(); pq.erase(pq.begin());
+        for (int i = 0, cnt = size(adj[cur]); i < cnt; i++) {
+            int nxt = adj[cur][i].first, ndist = dist[cur] + adj[cur][i].second;
+            if (ndist < dist[nxt]) dist[nxt] = ndist, dad[nxt] = cur, pq.insert(nxt);
         }
     }
 
-    return parent;
+    return pair<int*, int*>(dist, dad);
 }
--- code/graph/dijkstra.test.cpp
+++ code/graph/dijkstra.test.cpp
@@ -1,152 +1,32 @@
-#include <fstream>
-map<int, ii> res;
+#include "fstream"
 
-void test_1() {
-    int n = 2;
-    vvii adj_list(n);
-
-    res = dijkstra_path(0, adj_list);
-    assert_true(res.find(1) == res.end());
-    assert_true(res.find(0) != res.end());
-    assert_equal(0, res[0].first);
-    assert_equal(0, res[0].second);
-
-    res = dijkstra_path(1, adj_list);
-    assert_true(res.find(0) == res.end());
-    assert_true(res.find(1) != res.end());
-    assert_equal(0, res[1].first);
-    assert_equal(1, res[1].second);
-}
-
-void test_2() {
-    int n = 2;
-    vvii adj_list(n);
-    adj_list[0].push_back(ii(42, 1));
-
-    res = dijkstra_path(0, adj_list);
-    assert_true(res.find(1) != res.end());
-    assert_true(res.find(0) != res.end());
-    assert_equal(42, res[1].first);
-    assert_equal(0, res[1].second);
-    assert_equal(0, res[0].first);
-    assert_equal(0, res[0].second);
-
-    res = dijkstra_path(1, adj_list);
-    assert_true(res.find(0) == res.end());
-    assert_true(res.find(1) != res.end());
-    assert_equal(0, res[1].first);
-    assert_equal(1, res[1].second);
-}
-
-void test_3() {
-    int n = 2;
-    vvii adj_list(n);
-    adj_list[0].push_back(ii(42, 1));
-    adj_list[1].push_back(ii(42, 0));
-
-    res = dijkstra_path(0, adj_list);
-    assert_true(res.find(1) != res.end());
-    assert_true(res.find(0) != res.end());
-    assert_equal(42, res[1].first);
-    assert_equal(0, res[1].second);
-    assert_equal(0, res[0].first);
-    assert_equal(0, res[0].second);
-
-    res = dijkstra_path(1, adj_list);
-    assert_true(res.find(1) != res.end());
-    assert_true(res.find(0) != res.end());
-    assert_equal(0, res[1].first);
-    assert_equal(1, res[1].second);
-    assert_equal(42, res[0].first);
-    assert_equal(1, res[0].second);
-}
-
-void test_4() {
-    int n = 4;
-    vvii adj_list(n);
-    adj_list[0].push_back(ii(5, 1));
-    adj_list[1].push_back(ii(8, 2));
-    adj_list[1].push_back(ii(30, 3));
-    adj_list[2].push_back(ii(7, 3));
-
-    res = dijkstra_path(0, adj_list);
-    assert_true(res.find(1) != res.end());
-    assert_equal(5, res[1].first);
-    assert_equal(0, res[1].second);
-    assert_true(res.find(2) != res.end());
-    assert_equal(13, res[2].first);
-    assert_equal(1, res[2].second);
-    assert_true(res.find(3) != res.end());
-    assert_equal(20, res[3].first);
-    assert_equal(2, res[3].second);
-
-    res = dijkstra_path(3, adj_list);
-    assert_true(res.find(0) == res.end());
-    assert_true(res.find(1) == res.end());
-    assert_true(res.find(2) == res.end());
-
-    res = dijkstra_path(1, adj_list);
-    assert_true(res.find(3) != res.end());
-    assert_equal(15, res[3].first);
-}
-
-void test_5() {
-    int n = 4;
-    vvii adj_list(4);
-    adj_list[0].push_back(ii(4, 1));
-    adj_list[0].push_back(ii(5, 2));
-    adj_list[1].push_back(ii(2, 3));
-    adj_list[2].push_back(ii(-5, 1));
-
-    res = dijkstra_path(0, adj_list);
-    assert_true(res.find(0) != res.end());
-    assert_equal(0, res[0].first);
-    assert_true(res.find(2) != res.end());
-    assert_equal(5, res[2].first);
-    assert_equal(0, res[2].second);
-    assert_true(res.find(1) != res.end());
-    assert_equal(0, res[1].first);
-    assert_equal(2, res[1].second);
-    assert_true(res.find(3) != res.end());
-    assert_equal(2, res[3].first);
-    assert_equal(1, res[3].second);
-}
-
-void test_6() {
+/* Field testing: USACO butter, UVa 721 */
+void test() {
     ifstream ifs("dijkstra_fast.test.in");
     int ts;
     ifs >> ts;
     for (int t = 0; t < ts; t++) {
         int n, m;
         ifs >> n >> m;
-        vector<vector<ii> > adj(n);
+        vector<ii> *adj = new vector<ii>[n];
         for (int i = 0; i < m; i++) {
             int a, b, c;
             ifs >> a >> b >> c;
-            adj[a].push_back(ii(c, b));
+            adj[a].push_back(ii(b, c));
         }
 
         for (int i = 0; i < n; i++) {
-            map<int, ii> res = dijkstra_path(i, adj);
+            pair<int*, int*> res = dijkstra_fast(n, i, adj);
             for (int j = 0; j < n; j++) {
                 int correct;
                 ifs >> correct;
 
                 if (correct == -1) {
-                    assert_equal(INF, res.find(j) == res.end() ? INF : res[j].first);
+                    assert_equal(INF, res.first[j]);
                 } else {
-                    assert_equal(correct, res.find(j) == res.end() ? INF : res[j].first);
+                    assert_equal(correct, res.first[j]);
                 }
             }
         }
     }
-}
-
-void test() {
-    test_1();
-    test_2();
-    test_3();
-    test_4();
-    test_5();
-    test_6();
 }
