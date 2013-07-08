6fc9bef9e656a1453e3d0dc482cf0ba014b8ee0d
SuprDewd <suprdewd@gmail.com>
2013-07-08 09:14:31 +0000

    Shortened union find code.

--- code/data-structures/unionfind.cpp
+++ code/data-structures/unionfind.cpp
@@ -1,39 +1,13 @@
-class union_find {
-private:
-    int* parent;
+struct union_find {
+    vi parent;
     int cnt;
-
-public:
     union_find(int n) {
-        parent = new int[cnt = n];
-        for (int i = 0; i < cnt; i++)
-            parent[i] = i;
+        parent.resize(cnt = n);
+        for (int i = 0; i < cnt; i++) parent[i] = i;
     }
-
-    union_find(const union_find& other) {
-        parent = new int[cnt = other.cnt];
-        for (int i = 0; i < cnt; i++)
-            parent[i] = other.parent[i];
-    }
-
-    ~union_find() {
-        if (parent) {
-            delete[] parent;
-            parent = NULL;
-        }
-    }
-
-    int find(int i) {
-        assert(parent != NULL);
-        return parent[i] == i ? i : (parent[i] = find(parent[i]));
-    }
-
+    int find(int i) { return parent[i] == i ? i : (parent[i] = find(parent[i])); }
     bool unite(int i, int j) {
-        assert(parent != NULL);
-
-        int ip = find(i),
-            jp = find(j);
-
+        int ip = find(i), jp = find(j);
         parent[ip] = jp;
         return ip != jp;
     }
