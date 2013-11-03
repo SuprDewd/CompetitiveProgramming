3366d7cbf2b973ee1f3e5c0a0d7b07031640be1e
Helgi Kristvin Sigurbjarnarson <helgikrs@gmail.com>
2013-11-03 08:55:08 +0000

    Numeric integration

--- code/mathematics/numeric_integration.cpp
+++ code/mathematics/numeric_integration.cpp
@@ -1,7 +1,4 @@
-double integrate(double (*f)(double), double a, double b, double delta = 1) {
-    if(abs(a - b) < delta) {
-        return (b-a)/6 * (f(a) + 4 * f((a+b)/2) + f(b));
-    }
-
+double integrate(double (*f)(double), double a, double b, double delta = 1e-6) {
+    if(abs(a - b) < delta) return (b-a)/8 * (f(a) + 3*f((2*a+b)/3) + 3*f((a+2*b)/3) + f(b));
     return integrate(f, a, (a+b)/2, delta) + integrate(f, (a+b)/2, b, delta);
 }
