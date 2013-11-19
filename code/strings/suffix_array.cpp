8e63dc9654b87e7ad8f315e78b8ac6f3ba4ef26e
SuprDewd <suprdewd@gmail.com>
2013-11-19 06:13:19 +0000

    Added suffix array.

---
+++ code/strings/suffix_array.cpp
@@ -0,0 +1,25 @@
+struct entry { ii nr; int p; };
+bool operator <(const entry &a, const entry &b) { return a.nr < b.nr; }
+struct suffix_array {
+    string s; int n; vvi P; vector<entry> L; vi idx;
+    suffix_array(string s) : s(s), n(size(s)) {
+        L = vector<entry>(n), P.push_back(vi(n)), idx = vi(n);
+        for (int i = 0; i < n; i++) P[0][i] = s[i] - 'a';
+        for (int stp = 1, cnt = 1; cnt >> 1 < n; stp++, cnt <<= 1) {
+            P.push_back(vi(n));
+            for (int i = 0; i < n; i++)
+                L[L[i].p = i].nr = ii(P[stp - 1][i], i + cnt < n ? P[stp - 1][i + cnt] : -1);
+            sort(L.begin(), L.end());
+            for (int i = 0; i < n; i++)
+                P[stp][L[i].p] = i > 0 && L[i].nr == L[i - 1].nr ? P[stp][L[i - 1].p] : i;
+        }
+        for (int i = 0; i < n; i++) idx[P[size(P) - 1][i]] = i;
+    }
+    int lcp(int x, int y) {
+        int res = 0;
+        if (x == y) return n - x;
+        for (int k = size(P) - 1; k >= 0 && x < n && y < n; k--)
+            if (P[k][x] == P[k][y]) x += 1 << k, y += 1 << k, res += 1 << k;
+        return res;
+    }
+};
---
+++ code/strings/suffix_array.test.cpp
@@ -0,0 +1,104 @@
+
+/* Field testing: ICPC Live Archive 2755, ICPC Live Archive 4513, SPOJ DISUBSTR */
+
+struct cmp {
+    int *idx;
+    string s;
+    cmp(int *idx, string s) : idx(idx), s(s) { }
+    bool operator()(int a, int b) {
+        return s.substr(a) < s.substr(b);
+    }
+};
+
+class naive_suffix_array {
+private:
+    string s;
+    int n;
+public:
+    int *idx;
+
+    // naive_suffix_array(string s) : s(s + "\x0c"), n(size(s)+1) {
+    naive_suffix_array(string s) : s(s), n(size(s)) {
+        idx = new int[n];
+        for (int i = 0; i < n; i++) idx[i] = i;
+        sort(idx, idx + n, cmp(idx,s));
+    }
+};
+
+void make_sure_ok(string s) {
+    s = s + "~";
+    suffix_array sa1(s);
+    naive_suffix_array sa2(s);
+
+    // for (int i = 0; i < size(s); i++) {
+    //     cout << s.substr(sa1.idx[i]) << endl;
+    // }
+
+    // cout << endl;
+    // for (int i = 0; i < size(s); i++) {
+    //     cout << s.substr(sa2.idx[i]) << endl;
+    // }
+
+    // cout << endl;
+
+    for (int i = 0; i < size(s); i++) {
+
+        assert_equal(sa1.idx[i], sa2.idx[i]);
+
+        if (sa1.idx[i] != sa2.idx[i])
+        {
+            cout << s << endl;
+        }
+    }
+}
+
+void test() {
+    make_sure_ok("GATAGACA");
+    make_sure_ok("");
+    make_sure_ok("A");
+    make_sure_ok("AA");
+    make_sure_ok("AAA");
+    make_sure_ok("AAAA");
+    make_sure_ok("AAAAA");
+    make_sure_ok("AAAAAA");
+    make_sure_ok("AAAAAAA");
+    make_sure_ok("AAAAAAAA");
+    make_sure_ok("B");
+    make_sure_ok("BA");
+    make_sure_ok("BAA");
+    make_sure_ok("BAAA");
+    make_sure_ok("BAAAA");
+    make_sure_ok("BAAAAA");
+    make_sure_ok("BAAAAAA");
+    make_sure_ok("BAAAAAAA");
+    make_sure_ok("BAAAAAAAA");
+    make_sure_ok("AB");
+    make_sure_ok("AAB");
+    make_sure_ok("AAAB");
+    make_sure_ok("AAAAB");
+    make_sure_ok("AAAAAB");
+    make_sure_ok("AAAAAAB");
+    make_sure_ok("AAAAAAAB");
+    make_sure_ok("AAAAAAAAB");
+    make_sure_ok("AB");
+    make_sure_ok("ABA");
+    make_sure_ok("ABAA");
+    make_sure_ok("ABAAA");
+    make_sure_ok("ABAAAA");
+    make_sure_ok("ABAAAAA");
+    make_sure_ok("ABAAAAAA");
+    make_sure_ok("ABAAAAAAA");
+
+
+    for (int i = 0; i < 10000; i++) {
+        int len = rand() % 1000;
+        stringstream ss;
+        for (int j = 0; j < len; j++) {
+            ss << static_cast<char>('A' + (rand() % 26));
+        }
+
+        string s = ss.str();
+        // cout << s << endl;
+        make_sure_ok(s);
+    }
+}
