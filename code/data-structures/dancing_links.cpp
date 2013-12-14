feebfbce28f70025f2bd8fced5f66cd41a45fb15
SuprDewd <suprdewd@gmail.com>
2013-12-14 00:12:21 +0000

    Added the dancing links data structure.

---
+++ code/data-structures/dancing_links.cpp
@@ -0,0 +1,31 @@
+template <class T>
+struct dancing_links {
+    struct node {
+        T item;
+        node *l, *r;
+        node(const T &item, node *l = NULL, node *r = NULL) : item(item), l(l), r(r) {
+            if (l) l->r = this;
+            if (r) r->l = this;
+        }
+    };
+    node *front, *back;
+    dancing_links() { front = back = NULL; }
+    node *push_back(const T &item) {
+        back = new node(item, back, NULL);
+        if (!front) front = back;
+        return back;
+    }
+    node *push_front(const T &item) {
+        front = new node(item, NULL, front);
+        if (!back) back = front;
+        return front;
+    }
+    void erase(node *n) {
+        if (!n->l) front = n->r; else n->l->r = n->r;
+        if (!n->r) back = n->l; else n->r->l = n->l;
+    }
+    void restore(node *n) {
+        if (!n->l) front = n; else n->l->r = n;
+        if (!n->r) back = n; else n->r->l = n;
+    }
+};
