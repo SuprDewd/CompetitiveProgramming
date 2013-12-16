ecc65cee843708daf763a9e7a7d2ce275113daf2
SuprDewd <suprdewd@gmail.com>
2013-12-16 23:09:38 +0000

    Updated tests for dancing links.

--- code/data-structures/dancing_links.test.cpp
+++ code/data-structures/dancing_links.test.cpp
@@ -1,3 +1,37 @@
-void test() {
 
+vvi perms;
+vi cur;
+
+void rec(dancing_links<int> &left) {
+    if (left.front == NULL) {
+        perms.push_back(vi(cur));
+    } else {
+        for (dancing_links<int>::node *it = left.front; it; it = it->r) {
+            cur.push_back(it->item);
+            left.erase(it);
+            rec(left);
+            left.restore(it);
+            cur.pop_back();
+        }
+    }
+}
+
+void test() {
+    int N = 8;
+    dancing_links<int> left;
+    int *arr = new int[N];
+    for (int i = 0; i < N; i++) left.push_back(arr[i] = i + 1);
+    rec(left);
+    int at = 0;
+    do {
+        for (int i = 0; i < N; i++) {
+            assert_equal(arr[i], perms[at][i]);
+        }
+
+        at++;
+    } while(next_permutation(arr, arr + N));
+
+    assert(at == size(perms));
+
+    delete[] arr;
 }
