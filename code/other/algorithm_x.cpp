772497611449ea71372c0cee9fc19623bad7547c
SuprDewd <suprdewd@gmail.com>
2013-12-16 22:59:17 +0000

    Added Algorithm X.

---
+++ code/other/algorithm_x.cpp
@@ -0,0 +1,91 @@
+bool handle_solution(vi rows) { return false; }
+struct exact_cover {
+    struct node {
+        node *l, *r, *u, *d, *p;
+        int row, col, size;
+        node(int row, int col) : row(row), col(col) { size = 0; l = r = u = d = p = NULL; }
+    };
+    int rows, cols, *sol;
+    bool **arr;
+    node *head;
+    exact_cover(int rows, int cols) : rows(rows), cols(cols), head(NULL) {
+        arr = new bool*[rows];
+        sol = new int[rows];
+        for (int i = 0; i < rows; i++)
+            arr[i] = new bool[cols], memset(arr[i], 0, cols);
+    }
+    void set_value(int row, int col, bool val = true) { arr[row][col] = val; }
+    void setup() {
+        node ***ptr = new node**[rows + 1];
+        for (int i = 0; i <= rows; i++) {
+            ptr[i] = new node*[cols];
+            for (int j = 0; j < cols; j++)
+                if (i == rows || arr[i][j]) ptr[i][j] = new node(i, j);
+                else ptr[i][j] = NULL;
+        }
+        for (int i = 0; i <= rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (!ptr[i][j]) continue;
+                int ni = i + 1, nj = j + 1;
+                while (true) {
+                    if (ni == rows + 1) ni = 0;
+                    if (ni == rows || arr[ni][j]) break;
+                    ++ni;
+                }
+                ptr[i][j]->d = ptr[ni][j];
+                ptr[ni][j]->u = ptr[i][j];
+                while (true) {
+                    if (nj == cols) nj = 0;
+                    if (i == rows || arr[i][nj]) break;
+                    ++nj;
+                }
+                ptr[i][j]->r = ptr[i][nj];
+                ptr[i][nj]->l = ptr[i][j];
+            }
+        }
+        head = new node(rows, -1);
+        head->r = ptr[rows][0];
+        ptr[rows][0]->l = head;
+        head->l = ptr[rows][cols - 1];
+        ptr[rows][cols - 1]->r = head;
+        for (int j = 0; j < cols; j++) {
+            int cnt = -1;
+            for (int i = 0; i <= rows; i++)
+                if (ptr[i][j]) cnt++, ptr[i][j]->p = ptr[rows][j];
+            ptr[rows][j]->size = cnt;
+        }
+        for (int i = 0; i <= rows; i++) delete[] ptr[i];
+        delete[] ptr;
+    }
+    #define COVER(c, i, j) \
+        c->r->l = c->l, c->l->r = c->r; \
+        for (node *i = c->d; i != c; i = i->d) \
+            for (node *j = i->r; j != i; j = j->r) \
+                j->d->u = j->u, j->u->d = j->d, j->p->size--;
+    #define UNCOVER(c, i, j) \
+        for (node *i = c->u; i != c; i = i->u) \
+            for (node *j = i->l; j != i; j = j->l) \
+                j->p->size++, j->d->u = j->u->d = j; \
+        c->r->l = c->l->r = c;
+    bool search(int k = 0) {
+        if (head == head->r) {
+            vi res(k);
+            for (int i = 0; i < k; i++) res[i] = sol[i];
+            sort(res.begin(), res.end());
+            return handle_solution(res);
+        }
+        node *c = head->r, *tmp = head->r;
+        for ( ; tmp != head; tmp = tmp->r) if (tmp->size < c->size) c = tmp;
+        if (c == c->d) return false;
+        COVER(c, i, j);
+        bool found = false;
+        for (node *r = c->d; !found && r != c; r = r->d) {
+            sol[k] = r->row;
+            for (node *j = r->r; j != r; j = j->r) { COVER(j->p, a, b); }
+            found = search(k + 1);
+            for (node *j = r->l; j != r; j = j->l) { UNCOVER(j->p, a, b); }
+        }
+        UNCOVER(c, i, j);
+        return found;
+    }
+};
---
+++ code/other/algorithm_x.test.cpp
@@ -0,0 +1,10 @@
+void test() {
+    exact_cover ec(4, 3);
+    ec.set_value(0, 0);
+    ec.set_value(0, 1);
+    ec.set_value(1, 0);
+    ec.set_value(2, 1);
+    ec.set_value(3, 2);
+    ec.setup();
+    ec.search();
+}
