7179cb3d6b43dfc2dce2a4838bec5d8a31a800c5
SuprDewd <suprdewd@gmail.com>
2014-01-06 23:09:04 +0000

    Added euler path.

---
+++ code/graph/euler_path.cpp
@@ -0,0 +1,30 @@
+#define MAXV 1000
+#define MAXE 5000
+vi adj[MAXV];
+int n, m, indeg[MAXV], outdeg[MAXV], res[MAXE + 1];
+ii start_end() {
+    int start = -1, end = -1, any = 0, c = 0;
+    for (int i = 0; i < n; i++) {
+        if (outdeg[i] > 0) any = i;
+        if (indeg[i] + 1 == outdeg[i]) start = i, c++;
+        else if (indeg[i] == outdeg[i] + 1) end = i, c++;
+        else if (indeg[i] != outdeg[i]) return ii(-1,-1);
+    }
+    if ((start == -1) != (end == -1) || (c != 2 && c != 0)) return ii(-1,-1);
+    if (start == -1) start = end = any;
+    return ii(start, end);
+}
+bool euler_path() {
+    ii se = start_end();
+    int cur = se.first, at = m + 1;
+    if (cur == -1) return false;
+    stack<int> s;
+    while (true) {
+        if (outdeg[cur] == 0) {
+            res[--at] = cur;
+            if (s.empty()) break;
+            cur = s.top(); s.pop();
+        } else s.push(cur), cur = adj[cur][--outdeg[cur]];
+    }
+    return at == 0;
+}
