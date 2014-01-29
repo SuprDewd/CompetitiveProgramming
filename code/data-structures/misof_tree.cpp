5c350ff0b740560c273f73acef13f6a59ff3de8e
SuprDewd <suprdewd@gmail.com>
2014-01-29 01:04:20 +0000

    Added misof tree.

---
+++ code/data-structures/misof_tree.cpp
@@ -0,0 +1,13 @@
+#define BITS 15
+struct misof_tree {
+    int cnt[BITS][1<<BITS];
+    misof_tree() { memset(cnt, 0, sizeof(cnt)); }
+    void insert(int x) { for (int i = 0; i < BITS; cnt[i++][x]++, x >>= 1); }
+    void erase(int x) { for (int i = 0; i < BITS; cnt[i++][x]--, x >>= 1); }
+    int nth(int n) {
+        int res = 0;
+        for (int i = BITS-1; i >= 0; i--)
+            if (cnt[i][res <<= 1] <= n) n -= cnt[i][res], res |= 1;
+        return res;
+    }
+};
---
+++ code/data-structures/misof_tree.test.cpp
@@ -0,0 +1,37 @@
+#include "../code/data-structures/avl_tree.cpp"
+
+void test() {
+    misof_tree t1;
+    avl_tree<int> t2;
+
+    for (int i = 0; i < 1000000; i++) {
+        int op = rand() % 3;
+
+        if (op == 0) {
+            int x = rand() % (1 << BITS);
+
+            // printf("insert %d\n", x);
+
+            if (t2.find(x) == NULL)
+            {
+                t1.insert(x);
+                t2.insert(x);
+            }
+            else i--;
+
+        } else if (op == 1 && size(t2) > 0) {
+            int x = t2.nth(rand() % size(t2))->item;
+
+            // printf("erase %d\n", x);
+
+            t1.erase(x);
+            t2.erase(x);
+        } else if (op == 2 && size(t2) > 0) {
+            int n = rand() % size(t2);
+
+            // printf("nth %d: %d, %d\n", n, t2.nth(n)->item, t1.nth(n));
+
+            assert_equal(t2.nth(n)->item, t1.nth(n), true);
+        } else i--;
+    }
+}
