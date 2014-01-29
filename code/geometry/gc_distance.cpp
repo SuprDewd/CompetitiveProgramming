233431d0571a18104b1dea48bbf96aea180b8bb0
SuprDewd <suprdewd@gmail.com>
2014-01-29 00:23:02 +0000

    Simpler gc_distance.

--- code/geometry/gc_distance.cpp
+++ code/geometry/gc_distance.cpp
@@ -2,6 +2,7 @@ double gc_distance(double pLat, double pLong,
                   double qLat, double qLong, double r) {
     pLat *= pi / 180; pLong *= pi / 180;
     qLat *= pi / 180; qLong *= pi / 180;
-    return r * acos(cos(pLat) * cos(pLong) * cos(qLat) * cos(qLong) +
-                    cos(pLat) * sin(pLong) * cos(qLat) * sin(qLong) +
-                    sin(pLat) * sin(qLat)); }
+    return r * acos(cos(pLat) * cos(qLat) * cos(pLong - qLong) +
+                    sin(pLat) * sin(qLat));
+
+}
