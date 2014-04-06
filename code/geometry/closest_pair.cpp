ee545f1d219617538b5caaa5878ba4992d76b4ca
SuprDewd <suprdewd@gmail.com>
2014-04-06 22:36:45 +0000

    Added sweep line algorithm for closest pair of points.

---
+++ code/geometry/closest_pair.cpp
@@ -0,0 +1,20 @@
+#include "primitives.cpp"
+
+struct cmpx { bool operator ()(const point &a, const point &b) {
+        return abs(real(a) - real(b)) > EPS ?
+            real(a) < real(b) : imag(a) < imag(b); } };
+struct cmpy { bool operator ()(const point &a, const point &b) {
+    return abs(imag(a) - imag(b)) > EPS ?
+            imag(a) < imag(b) : real(a) < real(b); } };
+double closest_pair(vector<point> pts) {
+    sort(all(pts), cmpx());
+    set<point, cmpy> cur;
+    set<point, cmpy>::const_iterator it, jt;
+    double mn = INFINITY;
+    for (int i = 0, l = 0; i < size(pts); i++) {
+        while (real(pts[i]) - real(pts[l]) > mn) cur.erase(pts[l++]);
+        it = cur.lower_bound(point(-INFINITY, imag(pts[i]) - mn));
+        jt = cur.upper_bound(point(INFINITY, imag(pts[i]) + mn));
+        while (it != jt) mn = min(mn, abs(*it - pts[i])), it++;
+        cur.insert(pts[i]); }
+    return mn; }
---
+++ code/geometry/closest_pair.test.cpp
@@ -0,0 +1,3 @@
+void test() {
+    /* Field testing: Kattis closestpair1, Kattis closestpair2, UVa 10245, SPOJ CLOPPAIR */
+}
