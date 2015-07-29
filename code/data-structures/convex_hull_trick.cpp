a36b978a1ad1347562018279dbde8f925907cca6
SuprDewd <suprdewd@gmail.com>
2015-07-29 02:58:35 +0000

    Add convex hull trick

---
+++ code/data-structures/convex_hull_trick.cpp
@@ -0,0 +1,18 @@
+struct convex_hull_trick {
+    vector<pair<double,double> > h;
+    double intersect(int i) {
+        return (h[i+1].second-h[i].second)/(h[i].first-h[i+1].first); }
+    void add(double m, double b) {
+        h.push_back(make_pair(m,b));
+        while (size(h) >= 3) {
+            int n = size(h);
+            if (intersect(n-3) < intersect(n-2)) break;
+            swap(h[n-2], h[n-1]);
+            h.pop_back(); } }
+    double get_min(double x) {
+        int lo = 0, hi = size(h) - 2, res = -1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (intersect(mid) <= x) res = mid, lo = mid + 1;
+            else hi = mid - 1; }
+        return h[res+1].first * x + h[res+1].second; } };
