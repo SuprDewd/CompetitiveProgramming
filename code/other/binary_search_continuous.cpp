31b72a36ddacd00b0640f8ca0d0f772140dfa64e
SuprDewd <suprdewd@gmail.com>
2015-07-29 02:50:47 +0000

    Make notebook less verbose by default, and remove some redundant stuff

--- code/other/binary_search_continuous.cpp
+++
@@ -1,9 +0,0 @@
-double binary_search_continuous(double low, double high,
-        double eps, double (*f)(double)) {
-    while (true) {
-        double mid = (low + high) / 2, cur = f(mid);
-        if (abs(cur) < eps) return mid;
-        else if (0 < cur) high = mid;
-        else low = mid;
-    }
-}
