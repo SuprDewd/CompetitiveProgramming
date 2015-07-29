0ed6184fb2a73d7b9d56b8a39325d9d66b658b44
SuprDewd <suprdewd@gmail.com>
2015-07-29 21:18:13 +0000

    Added IDA* algorithm

---
+++ code/graph/idastar.test.cpp
@@ -0,0 +1,10 @@
+void test() {
+    // Field testing: UVa 10181, UVa 11163
+
+    n = 15;
+    int tc[15] = {
+        1, 3, 6, 9, 2, 14, 4, 13, 7, 11, 0, 12, 8, 5, 10
+    };
+    rep(i,0,n) cur[i] = tc[i];
+    assert(idastar() == 52);
+}
