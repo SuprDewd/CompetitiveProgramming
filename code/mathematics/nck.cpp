9dc4ae16b994641f7960942aebdad17e657128ff
SuprDewd <suprdewd@gmail.com>
2015-08-30 05:01:55 +0000

    Add Lucas' theorem

--- code/mathematics/nck.cpp
+++ code/mathematics/nck.cpp
@@ -1,6 +1,15 @@
 int nck(int n, int k) {
-    if (n - k < k) k = n - k;
+    if (n < k) return 0;
+    k = min(k, n - k);
     int res = 1;
     rep(i,1,k+1) res = res * (n - (k - i)) / i;
+    return res;
+}
+int nck(int n, int k, int p) {
+    int res = 1;
+    while (n || k) {
+        res *= nck(n % p, k % p);
+        res %= p, n /= p, k /= p;
+    }
     return res;
 }
--- code/mathematics/nck.test.cpp
+++ code/mathematics/nck.test.cpp
@@ -5,4 +5,15 @@ void test() {
     assert_equal(2, nck(2, 1));
     assert_equal(45, nck(10, 8));
     assert_equal(462, nck(11, 5));
+    rep(i,2,500) {
+        bool ok = true;
+        rep(j,2,i) if (i%j==0) ok = false;
+        if (!ok) continue;
+        assert_equal(1%i, nck(0, 0, i));
+        assert_equal(1%i, nck(1, 0, i));
+        assert_equal(1%i, nck(1, 1, i));
+        assert_equal(2%i, nck(2, 1, i));
+        assert_equal(45%i, nck(10, 8, i));
+        assert_equal(462%i, nck(11, 5, i));
+    }
 }
