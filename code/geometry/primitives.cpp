8ea32911aa99bde6a6ddb1a378b1242fb9c8ae20
James Elías <jamiees2@gmail.com>
2015-11-25 19:15:02 +0100

    partitioned geometry primitives into primitives, lines and circles

--- code/geometry/primitives.cpp
+++ code/geometry/primitives.cpp
@@ -13,11 +13,8 @@ point reflect(P(p), L(about1, about2)) {
 point proj(P(u), P(v)) { return dot(u, v) / dot(u, u) * u; }
 point normalize(P(p), double k = 1.0) {
     return abs(p) == 0 ? point(0,0) : p / abs(p) * k; }
-bool parallel(L(a, b), L(p, q)) { return abs(cross(b - a, q - p)) < EPS; }
 double ccw(P(a), P(b), P(c)) { return cross(b - a, c - b); }
 bool collinear(P(a), P(b), P(c)) { return abs(ccw(a, b, c)) < EPS; }
-bool collinear(L(a, b), L(p, q)) {
-    return abs(ccw(a, b, p)) < EPS && abs(ccw(a, b, q)) < EPS;  }
 double angle(P(a), P(b), P(c)) {
     return acos(dot(b - a, c - b) / abs(b - a) / abs(c - b)); }
 double signed_angle(P(a), P(b), P(c)) {
@@ -28,67 +25,5 @@ double progress(P(p), L(a, b)) {
     if (abs(real(a) - real(b)) < EPS)
         return (imag(p) - imag(a)) / (imag(b) - imag(a));
     else return (real(p) - real(a)) / (real(b) - real(a)); }
-bool intersect(L(a, b), L(p, q), point &res, bool segment = false) {
-    // NOTE: check for parallel/collinear lines before calling this function
-    point r = b - a, s = q - p;
-    double c = cross(r, s), t = cross(p - a, s) / c, u = cross(p - a, r) / c;
-    if (segment && (t < 0-EPS || t > 1+EPS || u < 0-EPS || u > 1+EPS))
-        return false;
-    res = a + t * r;
-    return true;
-}
-point closest_point(L(a, b), P(c), bool segment = false) {
-    if (segment) {
-        if (dot(b - a, c - b) > 0) return b;
-        if (dot(a - b, c - a) > 0) return a;
-    }
-    double t = dot(c - a, b - a) / norm(b - a);
-    return a + t * (b - a);
-}
-double line_segment_distance(L(a,b), L(c,d)) {
-    double x = INFINITY;
-    if (abs(a - b) < EPS && abs(c - d) < EPS) x = abs(a - c);
-    else if (abs(a - b) < EPS) x = abs(a - closest_point(c, d, a, true));
-    else if (abs(c - d) < EPS) x = abs(c - closest_point(a, b, c, true));
-    else if ((ccw(a, b, c) < 0) != (ccw(a, b, d) < 0) &&
-             (ccw(c, d, a) < 0) != (ccw(c, d, b) < 0)) x = 0;
-    else {
-        x = min(x, abs(a - closest_point(c,d, a, true)));
-        x = min(x, abs(b - closest_point(c,d, b, true)));
-        x = min(x, abs(c - closest_point(a,b, c, true)));
-        x = min(x, abs(d - closest_point(a,b, d, true)));
-    }
-    return x;
-}
-int intersect(C(A, rA), C(B, rB), point & res1, point & res2) {
-    double d = abs(B - A);
-    if ((rA + rB) <  (d - EPS) || d < abs(rA - rB) - EPS) return 0;
-    double a = (rA*rA - rB*rB + d*d) / 2 / d, h = sqrt(rA*rA - a*a);
-    point v = normalize(B - A, a), u = normalize(rotate(B-A), h);
-    res1 = A + v + u, res2 = A + v - u;
-    if (abs(u) < EPS) return 1; return 2;
-}
-int intersect(L(A, B), C(O, r), point & res1, point & res2) {
-     double h = abs(O - closest_point(A, B, O));
-     if(r < h - EPS) return 0;
-     point H = proj(O - A, B - A) + A, v = normalize((B - A), sqrt(r*r - h*h));
-     res1 = H + v; res2 = H - v;
-     if(abs(v) < EPS) return 1; return 2;
- }
-int tangent(P(A), C(O, r), point & res1, point & res2) {
-    point v = O - A; double d = abs(v);
-    if (d < r - EPS) return 0;
-    double alpha = asin(r / d), L = sqrt(d*d - r*r);
-    v = normalize(v, L);
-    res1 = A + rotate(v, alpha); res2 = A + rotate(v, -alpha);
-    if (abs(r - d) < EPS || abs(v) < EPS) return 1;
-    return 2;
-}
-void tangent_outer(point A, double rA, point B, double rB, PP(P), PP(Q)) {
-    if (rA - rB > EPS) { swap(rA, rB); swap(A, B); }
-    double theta = asin((rB - rA)/abs(A - B));
-    point v = rotate(B - A, theta + pi/2), u = rotate(B - A, -(theta + pi/2));
-    u = normalize(u, rA);
-    P.first = A + normalize(v, rA); P.second = B + normalize(v, rB);
-    Q.first = A + normalize(u, rA); Q.second = B + normalize(u, rB);
-}
+
+
--- code/geometry/primitives.test.cpp
+++ code/geometry/primitives.test.cpp
@@ -10,24 +10,6 @@ void assert_equal(point a, point b) {
 void test() {
     /* Field testing: UVa10263, Kattis segmentdistance */
 
-    point a, b, c;
-
-    a = point(-2, -2); b = point(3, 3); c = point(5, 5);
-    assert_true(collinear(a, b, c));
-    assert_true(collinear(a, c, b));
-    assert_true(collinear(b, a, c));
-    assert_true(collinear(b, c, a));
-    assert_true(collinear(c, a, b));
-    assert_true(collinear(c, b, a));
-
-    a = point(-2, 2); b = point(3, 3); c = point(5, 5);
-    assert_false(collinear(a, b, c));
-    assert_false(collinear(a, c, b));
-    assert_false(collinear(b, a, c));
-    assert_false(collinear(b, c, a));
-    assert_false(collinear(c, a, b));
-    assert_false(collinear(c, b, a));
-
     assert_equal(point(-3, 3), rotate(point(3, 3), pi / 2, point(0,0)));
     assert_equal(point(-3, 3), rotate(point(3, 3)));
     assert_equal(point(-3, -3), rotate(point(3, 3), pi, point(0,0)));
@@ -43,88 +25,5 @@ void test() {
     assert_equal(point(0,-1), normalize(point(0,-2)));
     assert_equal(point(cos(pi/3), sin(pi/3)), normalize(point(cos(pi/3) * 2, sin(pi/3) * 2)));
 
-    assert_equal(point(1, 1), closest_point(point(2, 2), point(3, 3), point(2, 0)));
-    assert_equal(point(0.5, 0.5), closest_point(point(2, 2), point(3, 3), point(1, 0)));
-    assert_equal(point(1, 1), closest_point(point(2, 2), point(3, 3), point(0, 2)));
-    assert_equal(point(0.5, 0.5), closest_point(point(2, 2), point(3, 3), point(0, 1)));
-    assert_equal(point(0, 0), closest_point(point(2, 2), point(3, 3), point(0, 0)));
-    assert_equal(point(2, 2), closest_point(point(2, 2), point(3, 3), point(2, 2)));
-    assert_equal(point(1, 1), closest_point(point(2, 2), point(3, 3), point(1, 1)));
-    assert_equal(point(3, 3), closest_point(point(2, 2), point(3, 3), point(3, 3)));
-    assert_equal(point(2.5, 2.5), closest_point(point(2, 2), point(3, 3), point(2.5, 2.5)));
-
-    assert_equal(point(2.5, 2.5), closest_point(point(2, 2), point(3, 3), point(2.5, 2.5), true));
-    assert_equal(point(2, 2), closest_point(point(2, 2), point(3, 3), point(2, 2), true));
-    assert_equal(point(3, 3), closest_point(point(2, 2), point(3, 3), point(3, 3), true));
-    assert_equal(point(2, 2), closest_point(point(2, 2), point(3, 3), point(1, 1), true));
-    assert_equal(point(3, 3), closest_point(point(2, 2), point(3, 3), point(4, 4), true));
-    assert_equal(point(3, 3), closest_point(point(2, 2), point(3, 3), point(50, 100), true));
-    assert_equal(point(3, 3), closest_point(point(2, 2), point(3, 3), point(100, 50), true));
-    assert_equal(point(2, 2), closest_point(point(2, 2), point(3, 3), point(-50, -100), true));
-    assert_equal(point(2, 2), closest_point(point(2, 2), point(3, 3), point(-100, -50), true));
-
     assert_equal(point(6.0/25, -8.0/25), proj(point(-3, 4), point(2, 1)));
-
-    point res1, res2;
-    // Circle circle intersection
-    assert_equal(intersect(point(0,0), 2, point(3,0), 2, res1, res2), 2);
-    assert_equal(intersect(point(0,0), 2, point(3,2), 2, res1, res2), 2);
-    assert_equal(intersect(point(0,0), 2, point(2,3), 2, res1, res2), 2);
-    assert_equal(intersect(point(0,0), 2, point(0,3), 2, res1, res2), 2);
-    assert_equal(intersect(point(0,0), 2, point(-2,3), 2, res1, res2), 2);
-    assert_equal(intersect(point(0,0), 2, point(-3,2), 2, res1, res2), 2);
-    assert_equal(intersect(point(0,0), 2, point(-3,0), 2, res1, res2), 2);
-    assert_equal(intersect(point(0,0), 2, point(-3,-2), 2, res1, res2), 2);
-    assert_equal(intersect(point(0,0), 2, point(-2,-3), 2, res1, res2), 2);
-    assert_equal(intersect(point(0,0), 2, point(0,3), 2, res1, res2), 2);
-
-    assert_equal(intersect(point(0,0), 2, point(-3,2), 2, res1, res2), 2);
-    assert_equal(intersect(point(0,0), 2, point(-2,3), 2, res1, res2), 2);
-    assert_equal(intersect(point(0,0), 2, point(3,0), 2, res1, res2), 2);
-
-    assert_equal(intersect(point(1000,0), 2, point(1003,0), 2, res1, res2), 2);
-    assert_equal(intersect(point(0,1000), 2, point(3,1002), 2, res1, res2), 2);
-    assert_equal(intersect(point(0,100000), 2, point(2,100003), 2, res1, res2), 2);
-    assert_equal(intersect(point(-1000,0), 2, point(-1000,3), 2, res1, res2), 2);
-    assert_equal(intersect(point(0,-1000), 2, point(-2,-1003), 2, res1, res2), 2);
-    assert_equal(intersect(point(0,0), 2, point(-3,2), 2, res1, res2), 2);
-
-    assert_equal(intersect(point(0,0), 1, point(2,0), 1, res1, res2), 1);
-    assert_equal(intersect(point(0,0), 1, point(-2,0), 1, res1, res2), 1);
-
-    assert_equal(intersect(point(0,0), 1, point(4,0), 1, res1, res2), 0);
-    assert_equal(intersect(point(0,0), 1, point(-2.3048,10034), 1, res1, res2), 0);
-    assert_equal(intersect(point(0,0), 1, point(-823.847,2.7872), 1, res1, res2), 0);
-    assert_equal(intersect(point(0,0), 1, point(-972.66283,0), 1, res1, res2), 0);
-    assert_equal(intersect(point(0,0), 1, point(-20973249,2938490.02983942), 1, res1, res2), 0);
-
-    // Circle line intersection
-    assert_equal(2, intersect(point(-5,0), point(5,0), point(0,0), 3, res1, res2));
-    assert_equal(2, intersect(point(0,-5), point(0,5), point(0,0), 3, res1, res2));
-
-    assert_equal(0, intersect(point(-5,0), point(5,0), point(0,5), 3, res1, res2));
-    assert_equal(0, intersect(point(0,-5), point(0,5), point(5,0), 3, res1, res2));
-
-    assert_equal(1, intersect(point(-5,0), point(5,0), point(0,3), 3, res1, res2));
-    assert_equal(1, intersect(point(0,-5), point(0,5), point(3, -3234.234), 3, res1, res2));
-
-    // Point circle tangent
-    point M, N;
-    assert_equal(2, tangent(point(0,2), point(2,0), 2.0, M, N));
-    assert_equal(N, point(0,0));
-    assert_equal(M, point(2,2));
-    assert_equal(1, tangent(point(0,0), point(2,0), 2.0, M, N));
-    assert_equal(0, tangent(point(3.4142135,1.4142135), point(2,0), 2.0, M, N));
-
-    // Circle circle tangent
-    pair<point, point> res3, res4;
-    tangent_outer(point(0,1), 1.0, point(8,1), 1.0, res3, res4);
-    assert_equal(res3.first, point(0.0,2.0));
-    assert_equal(res3.second, point(8.0,2.0));
-    assert_equal(res4.first, point(0.0,0.0));
-    assert_equal(res4.second, point(8.0,0.0));
-
-    tangent_outer(point(0,1), 1.0, point(8,4), 4.0, res3, res4);
-    assert_equal(res4.first, point(0.0,0.0));
-    assert_equal(res4.second, point(8.0,0.0));
 }
