ed49ff4597fdbac98bedeb8ad8b51309964ed876
SuprDewd <suprdewd@gmail.com>
2015-11-25 20:26:02 +0100

    Added code for finding a primitive root

---
+++ code/mathematics/primitive_root.cpp
@@ -0,0 +1,12 @@
+#include "mod_pow.cpp"
+ll primitive_root(ll m) {
+    vector<ll> div;
+    for (ll i = 1; i*i <= m-1; i++) {
+        if ((m-1) % i == 0) {
+            if (i < m) div.push_back(i);
+            if (m/i < m) div.push_back(m/i); } }
+    rep(x,2,m) {
+        bool ok = true;
+        iter(it,div) if (mod_pow(x, *it, m) == 1) { ok = false; break; }
+        if (ok) return x; }
+    return -1; }
