00a400e72c318b5c459e03dd4e75950abdf8c11a
SuprDewd <suprdewd@gmail.com>
2015-11-25 20:22:34 +0100

    Add summatory phi and moebius functions

---
+++ code/mathematics/mertens.cpp
@@ -0,0 +1,19 @@
+#define L 9000000
+int mob[L], mer[L];
+unordered_map<ll,ll> mem;
+ll M(ll n) {
+    if (n < L) return mer[n];
+    if (mem.find(n) != mem.end()) return mem[n];
+    ll ans = 0, done = 1;
+    for (ll i = 2; i*i <= n; i++) ans += M(n/i), done = i;
+    for (ll i = 1; i*i <= n; i++) ans += mer[i] * (n/i - max(done, n/(i+1)));
+    return mem[n] = 1 - ans; }
+void sieve() {
+    for (int i = 1; i < L; i++) mer[i] = mob[i] = 1;
+    for (int i = 2; i < L; i++) {
+        if (mer[i]) {
+            mob[i] = -1;
+            for (int j = i+i; j < L; j += i)
+                mer[j] = 0, mob[j] = (j/i)%i == 0 ? 0 : -mob[j/i];
+        }
+        mer[i] = mob[i] + mer[i-1]; } }
---
+++ code/mathematics/summatory_phi.cpp
@@ -0,0 +1,17 @@
+#define N 10000000
+ll sp[N];
+unordered_map<ll,ll> mem;
+ll sumphi(ll n) {
+    if (n < N) return sp[n];
+    if (mem.find(n) != mem.end()) return mem[n];
+    ll ans = 0, done = 1;
+    for (ll i = 2; i*i <= n; i++) ans += sumphi(n/i), done = i;
+    for (ll i = 1; i*i <= n; i++) ans += sp[i] * (n/i - max(done, n/(i+1)));
+    return mem[n] = n*(n+1)/2 - ans; }
+void sieve() {
+    for (int i = 1; i < N; i++) sp[i] = i;
+    for (int i = 2; i < N; i++) {
+        if (sp[i] == i) {
+            sp[i] = i-1;
+            for (int j = i+i; j < N; j += i) sp[j] -= sp[j] / i; }
+        sp[i] += sp[i-1]; } }
