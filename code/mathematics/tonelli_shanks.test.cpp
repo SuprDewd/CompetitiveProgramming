1d72db34d5995ac5e3e636e06dc0e53d08ed897c
SuprDewd <suprdewd@gmail.com>
2015-12-19 23:50:05 +0000

    Added the Tonelli-Shanks algorithm

---
+++ code/mathematics/tonelli_shanks.test.cpp
@@ -0,0 +1,23 @@
+#include "../code/mathematics/is_prime.cpp"
+void test() {
+    // Field testing: Project Euler 437
+    rep(it,0,10000) {
+        int n;
+        while (true) {
+            n = randint(2, 100000);
+            if (is_prime(n)) {
+                break;
+            }
+        }
+        int a;
+        while (true) {
+            a = randint(1,n-1);
+            if (legendre(a, n) == 1) {
+                break;
+            }
+        }
+        ll res = tonelli_shanks(a, n);
+        assert_false(res == -1);
+        assert_equal<ll>(a, (res*res)%n);
+    }
+}
