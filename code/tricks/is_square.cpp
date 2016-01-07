850d9fa3052ee507b9351fd0a2d960bf1ea990d1
SuprDewd <suprdewd@gmail.com>
2016-01-07 23:47:29 +0000

    Add is_square function

---
+++ code/tricks/is_square.cpp
@@ -0,0 +1,11 @@
+long long M;
+void init_is_square() {
+    rep(i,0,64) M |= 1ULL << (63-(i*i)%64); }
+inline bool is_square(ll x) {
+    if ((M << x) >= 0) return false;
+    int c = __builtin_ctz(x);
+    if (c & 1) return false;
+    x >>= c;
+    if ((x&7) - 1) return false;
+    ll r = sqrt(x);
+    return r*r == x; }
---
+++ code/tricks/is_square.test.cpp
@@ -0,0 +1,11 @@
+void test() {
+    init_is_square();
+
+    rep(i,0,100000) {
+        int n = rand() % 100000000;
+        int st = sqrt(n);
+        while (st*st > n) st--;
+        while (st*st < n) st++;
+        assert_equal(st*st == n, is_square(n));
+    }
+}
