de225dd8bd6701c685dcc7036eb46c8c71a96b44
SuprDewd <suprdewd@gmail.com>
2016-03-25 17:35:49 +0000

    Simplify fraction code

--- code/mathematics/fraction.cpp
+++ code/mathematics/fraction.cpp
@@ -1,13 +1,12 @@
 template <class T> struct fraction {
     T gcd(T a, T b) { return b == T(0) ? a : gcd(b, a % b); }
     T n, d;
-    fraction(T n_, T d_) {
+    fraction(T n_=T(0), T d_=T(1)) {
         assert(d_ != 0);
         n = n_, d = d_;
         if (d < T(0)) n = -n, d = -d;
         T g = gcd(abs(n), abs(d));
         n /= g, d /= g; }
-    fraction(T n_) : n(n_), d(1) { }
     fraction(const fraction<T>& other) : n(other.n), d(other.d) { }
     fraction<T> operator +(const fraction<T>& other) const {
         return fraction<T>(n * other.d + other.n * d, d * other.d);}
