29bbc1a67c43a1b1324bd60b44496fbe1cc71277
SuprDewd <suprdewd@gmail.com>
2016-05-06 18:22:13 +0000

    Add constructive König's theorem

---
+++ code/graph/bipartite_mvc.cpp
@@ -0,0 +1,14 @@
+#include "hopcroft_karp.cpp"
+vector<bool> alt;
+void dfs(bipartite_graph &g, int at) {
+    alt[at] = true;
+    iter(it,g.adj[at]) {
+        alt[*it + g.N] = true;
+        if (g.R[*it] != -1 && !alt[g.R[*it]]) dfs(g, g.R[*it]); } }
+vi mvc_bipartite(bipartite_graph &g) {
+    vi res; g.maximum_matching();
+    alt.assign(g.N + g.M,false);
+    rep(i,0,g.N) if (g.L[i] == -1) dfs(g, i);
+    rep(i,0,g.N) if (!alt[i]) res.push_back(i);
+    rep(i,0,g.M) if (alt[g.N + i]) res.push_back(g.N + i);
+    return res; }
---
+++ code/graph/bipartite_mvc.test.cpp
@@ -0,0 +1,33 @@
+/* Field testing: Kattis sensor */
+void test() {
+    rep(ite,0,10000) {
+        int n = rand() % 400 + 1,
+            m = rand() % 400 + 1;
+        int k = rand() % n + 1;
+        vii es;
+        rep(i,0,n) {
+            rep(j,0,m) {
+                if (rand() % k == 0) {
+                    es.push_back(ii(i,j));
+                }
+            }
+        }
+        bipartite_graph g(n,m);
+        iter(it,es) {
+            g.add_edge(it->first, it->second);
+        }
+        int sz = g.maximum_matching();
+        vector<int> res = mvc_bipartite(g);
+        set<int> cover;
+        iter(it,res) cover.insert(*it);
+        assert(size(res) == sz);
+        int LEFT = 0,
+            RIGHT = n;
+        rep(i,0,size(es)) {
+            int a = LEFT + es[i].first,
+                b = RIGHT + es[i].second;
+            assert(cover.find(a) != cover.end() ||
+                   cover.find(b) != cover.end());
+        }
+    }
+}
