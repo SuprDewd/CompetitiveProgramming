cbc37d53e9387e82741ba83d750a8624f3725e7d
SuprDewd <suprdewd@gmail.com>
2016-05-24 00:39:38 +0000

    Fix circle tangent

--- code/geometry/circles.cpp
+++ code/geometry/circles.cpp
@@ -11,14 +11,14 @@ int intersect(L(A, B), C(O, r), point &r1, point &r2) {
     if (r < h - EPS) return 0;
     point v = normalize(B-A, sqrt(r*r - h*h));
     r1 = H + v, r2 = H - v;
-    return 1 + (abs(v) >= EPS); }
+    return 1 + (abs(v) > EPS); }
 int tangent(P(A), C(O, r), point &r1, point &r2) {
     point v = O - A; double d = abs(v);
     if (d < r - EPS) return 0;
     double alpha = asin(r / d), L = sqrt(d*d - r*r);
     v = normalize(v, L);
     r1 = A + rotate(v, alpha), r2 = A + rotate(v, -alpha);
-    return 1 + (abs(r - d) >= EPS && abs(v) >= EPS); }
+    return 1 + (abs(v) > EPS); }
 void tangent_outer(point A, double rA, point B, double rB, PP(P), PP(Q)) {
     if (rA - rB > EPS) { swap(rA, rB); swap(A, B); }
     double theta = asin((rB - rA)/abs(A - B));
