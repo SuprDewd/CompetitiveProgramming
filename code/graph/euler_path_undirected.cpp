e1d7c8f9d70a9d021cf42b49557b332b376ba943
Bjarki Ágúst Guðmundsson <suprdewd@gmail.com>
2016-10-07 23:41:22 +0000

    Add undirected euler cycle

---
+++ code/graph/euler_path_undirected.cpp
@@ -0,0 +1,21 @@
+multiset<int> adj[1010];
+list<int> L;
+list<int>::iterator euler(int at, int to, list<int>::iterator it) {
+    if (at == to) return it;
+    L.insert(it, at), --it;
+    while (!adj[at].empty()) {
+        int nxt = *adj[at].begin();
+        adj[at].erase(adj[at].find(nxt));
+        adj[nxt].erase(adj[nxt].find(at));
+        if (to == -1) {
+            it = euler(nxt, at, it);
+            L.insert(it, at);
+            --it;
+        } else {
+            it = euler(nxt, to, it);
+            to = -1;
+        }
+    }
+    return it;
+}
+// euler(0,-1,L.begin())
---
+++ code/graph/euler_path_undirected.test.cpp
@@ -0,0 +1,3 @@
+void test() {
+    // Field testing: Codeforces GYM 100431, Achromatic Number
+}
