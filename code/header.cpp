9cd63476c5f72c3edaf5bff5e27e4eac4679ccc4
Bjarki Ágúst Guðmundsson <bjarki.agust@gmail.com>
2019-05-25 00:23:58 +0000

    Fix GCC pragmas. Thanks @simonlindholm

--- code/header.cpp
+++ code/header.cpp
@@ -1,6 +1,5 @@
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
-#pragma GCC optimization ("unroll-loops")
+#pragma GCC optimize("Ofast","unroll-loops")
+#pragma GCC target("avx2,fma")
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i,a,b) for (__typeof(a) i=(a); i<(b); ++i)
