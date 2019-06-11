1093c14013eb38a27b99a58cdc9b7f951c0e21ee
Bjarki Ágúst Guðmundsson <bjarki.agust@gmail.com>
2019-06-11 09:40:27 +0000

    Check negative cycles in Floyd-Warshall

--- code/graph/floyd_warshall.cpp
+++ code/graph/floyd_warshall.cpp
@@ -1,5 +1,9 @@
 void floyd_warshall(int** arr, int n) {
   rep(k,0,n) rep(i,0,n) rep(j,0,n)
     if (arr[i][k] != INF && arr[k][j] != INF)
-      arr[i][j] = min(arr[i][j], arr[i][k] + arr[k][j]); }
+      arr[i][j] = min(arr[i][j], arr[i][k] + arr[k][j]);
+  // Check negative cycles
+  rep(i,0,n) rep(j,0,n) rep(k,0,n)
+    if (arr[i][k] != INF && arr[k][k] < 0 && arr[k][j]!=INF)
+      arr[i][j] = -INF; }
 // vim: cc=60 ts=2 sts=2 sw=2:
